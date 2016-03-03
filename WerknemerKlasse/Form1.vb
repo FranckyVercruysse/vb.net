11bec18e373cdbe5e56f85976b6ae7c56f21731c
Francky Vercruysse <francky.vercruysse@outlook.com>
2016-03-03 17:42:02 +0100

    Dit is een voorbeeld van OOP in vb.net

---
+++ WerknemerKlasse/Form1.vb
@@ -0,0 +1,29 @@
+﻿Public Class Form1
+    Dim eenWerknemer As Werknemer
+    Dim fotoPad As String
+
+    Private Sub Form1_Click(sender As Object, e As EventArgs) Handles Me.Click
+        MaakWerknemer("Jonas", "Vercruysse", "Administratie", "Dit is een ervaren programmeur", 0)
+    End Sub
+
+    Private Sub MaakWerknemer(Voornaam As String, Familienaam As String, Afdeling As String, Aantekeningen As String, Status As Integer)
+        If MsgBox("Wilt u een foto toevoegen ?", MsgBoxStyle.YesNo, "Foto toevoegen") = MsgBoxResult.Yes Then
+            OFDFoto.ShowDialog()
+        End If
+
+        If fotoPad = "" Then
+            eenWerknemer = New Werknemer(Me.CreateGraphics, Voornaam, Familienaam, Afdeling, Aantekeningen, Status)
+        Else
+            Dim oImg As Image = Image.FromFile(fotoPad)
+            eenWerknemer = New Werknemer(Me.CreateGraphics, Voornaam, Familienaam, Afdeling, Aantekeningen, Status, oImg)
+        End If
+
+        If eenWerknemer IsNot Nothing Then
+            eenWerknemer.Draw()
+        End If
+    End Sub
+
+    Private Sub OFDFoto_FileOk(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles OFDFoto.FileOk
+        fotoPad = OFDFoto.FileName
+    End Sub
+End Class
---
+++ WerknemerKlasse/Werknemer.vb
@@ -0,0 +1,69 @@
+﻿Imports System.Drawing
+
+Public Class Werknemer
+    'om drawing surface te beheren
+    Private G As Graphics
+    Private FotoBox As New Rectangle(5, 5, 150, 200)
+    Private InfoBox As New Rectangle(160, 5, 250, 200)
+
+    'Werknemer PROPERTIES
+    Public Voornaam As String
+    Public Naam As String
+    Public Afdeling As String
+    Public Aantekeningen As String
+    Public Status As WerknemerStatus
+    'Foto van de werknemer
+    Public Foto As Image
+
+
+    Public Enum WerknemerStatus
+        Actief
+        Beëindigd
+    End Enum
+
+    'wanneer een nieuwe instance van de klasse Werknemer gemaakt wordt... (constructor)
+    Public Sub New(GFX As Graphics, VNaam As String, FamilieNaam As String, Afd As String, Aantek As String, WerknStatus As Integer, Optional WerknemerFoto As Image = Nothing)
+        G = GFX
+
+        Voornaam = VNaam
+        Naam = FamilieNaam
+        Afdeling = Afd
+        Aantekeningen = Aantek
+
+        Select Case WerknStatus
+            Case 0
+                Status = WerknemerStatus.Actief
+            Case 1
+                Status = WerknemerStatus.Beëindigd
+            Case Else
+                Status = WerknemerStatus.Actief
+
+        End Select
+
+        Foto = WerknemerFoto
+    End Sub
+
+    Public Sub Draw()
+        'Wis Drawing Surface
+        G.Clear(Color.FromName("Control"))      'kleur van form Form1
+
+        'Werknemer foto box
+        G.FillRectangle(Brushes.AliceBlue, FotoBox)
+        G.DrawRectangle(Pens.Black, FotoBox)
+
+        If Foto IsNot Nothing Then
+            G.DrawImage(Foto, New Rectangle(FotoBox.X + 1, FotoBox.Y + 1, FotoBox.Width - 1, FotoBox.Height - 1))
+
+        End If
+
+        'Info Werknemer
+        G.FillRectangle(Brushes.Lavender, InfoBox)
+        G.DrawRectangle(Pens.Black, InfoBox)
+
+        G.DrawString("" &
+            "Naam Werknemer : " & Voornaam & " " & Naam & vbCrLf &
+            "Status Werknemer : " & Status.ToString & vbCrLf &
+            "Afdeling : " & Afdeling & vbCrLf &
+            "Aantekening : " & Aantekeningen, New Font("Tahoma", 9), Brushes.Black, New Point(InfoBox.X + 5, InfoBox.Y + 5))
+    End Sub
+End Class
